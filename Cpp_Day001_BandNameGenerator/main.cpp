c27c23b90480bdc4bc5fd3a37d15f02e9c4fcb22
travisback <travisback@icloud.com>
2022-06-05 00:13:43 -0700

    Initial Commit

---
+++ Cpp_Day001_BandNameGenerator/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Cpp_Day001_BandNameGenerator
+//
+//  Created by Travis Back on 6/5/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
